fcda7e69087e40ca6d5176cb2daa0b4a083c9c55
Mackenzie <mvlofthus@gmail.com>
2020-09-28 22:03:12 -0700

    wave 3 with Chris's video

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,16 +4,25 @@
 require_relative 'card'
 
 class Deck
+  attr
 
   def initialize
-
+    @cards = []
+    [:hearts, :spades, :clubs, :diamonds].each do |suit|
+      (1..13).each do |value|
+        card = Card.new(value, suit)
+        @cards << Card.new(value, suit)
+      end
+    end
   end
 
   def draw
-    # returns a card
+    drawn_card = @cards.pop
+    return drawn_card
   end
 
   def shuffle
-    # shuffles the deck
+    shuffled_deck = @cards.shuffle
+    return shuffled_deck
   end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,5 +5,45 @@ describe Deck do
   # What should you test?
     # 1.  Expected Behavior
     # 2.  Edge cases
+  before do
+    @deck = Deck.new
+  end
+
+  describe "constructor" do
+
+    it "can be instantiated" do
+      expect(@deck).must_be_instance_of Deck
+    end
+
+
+    describe "draw method" do
+      it "must respond to draw method" do
+        expect(@deck).must_respond_to :draw
+      end
+
+      it "returns a Card" do
+        expect(@deck.draw).must_be_instance_of Card
+      end
+
+      it "will let me draw 52 cards from an initial deck" do
+        52.times do
+          expect(@deck.draw).must_be_instance_of Card
+        end
+      end
+
+      it "will return nil" do #would be the place for a custom error
+      52.times do
+        expect(@deck.draw).must_be_instance_of Card
+      end
+
+      expect(@deck.draw).must_be_nil
+
+      end
+
+
+    end
+
+  end
 
 end
+
