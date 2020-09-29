0f590f0320d9ceac2c7c069a0e63954cea9b5670
Mackenzie <mvlofthus@gmail.com>
2020-09-28 20:56:03 -0700

    wave 1 and 2

--- lib/card.rb
+++ lib/card.rb
@@ -1,18 +1,45 @@
 
 # card.rb
 
+VALID_SUIT = [:diamonds, :clubs, :hearts,:spades]
+
+
 class Card
   attr_reader :value, :suit
 
   def initialize(value, suit)
-    @value = value
-    @suit = suit
-    
 
+    @value = value.to_i
+      raise ArgumentError.new("not a valid value") if value <1 || value > 13
+    @suit = suit.to_sym
+      raise ArgumentError.new("not a valid suit") if VALID_SUIT.include?(suit) == false
   end
 
   def to_s
-    return "#{value} of #{suit.to_s}"
+    face_cards = {
+        1 => "Ace",
+        11 => "Jack",
+        12 => "Queen",
+        13 => "King"
+    }
+
+    case value
+    when 1, 11, 12, 13
+      return "#{face_cards[value]} of #{suit.to_s}"
+    else
+      return "#{value} of #{suit.to_s}"
+    end
   end
 
+
+
 end
+
+
+# "#{four_of_hearts = Card.new(4, :hearts)
+#
+# puts four_of_hearts
+#
+# two_of_diamonds = Card.new(2, :diamonds)
+#
+# print two_of_diamonds}"
